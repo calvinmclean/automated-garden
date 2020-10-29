2468dced45ad083d8e5c1038c63f6f24c6dabaa7
Calvin McLean <calvinlmc@gmail.com>
2020-10-29 16:43:40 -0700

    Rearrange ESP32 pins

--- esp32_pins.h
+++ esp32_pins.h
@@ -1,14 +1,14 @@
 #ifndef esp32_pins
 #define esp32_pins
 
-#define VALVE_1_PIN 22
-#define VALVE_2_PIN 21
-#define VALVE_3_PIN 19
-#define PUMP_PIN 23
+#define VALVE_1_PIN 16
+#define VALVE_2_PIN 17
+#define VALVE_3_PIN 5
+#define PUMP_PIN 18
 
-#define BUTTON_1_PIN 18
-#define BUTTON_2_PIN 5
-#define BUTTON_3_PIN 17
-#define STOP_BUTTON_PIN 16
+#define BUTTON_1_PIN 19
+#define BUTTON_2_PIN 21
+#define BUTTON_3_PIN 22
+#define STOP_BUTTON_PIN 23
 
 #endif
