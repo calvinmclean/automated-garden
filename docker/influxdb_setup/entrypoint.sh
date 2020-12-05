e952e16adc545e358d3c05d74a996ab755182f48
Calvin McLean <calvinlmc@gmail.com>
2020-12-05 15:46:18 -0700

    Fix InfluxDB setup when restarting containers
    When restarting existing containers, the InfluxDB setup container would
    continuously fail and keep retrying. I added a ping command to check if it is
    ready rather than running until `influxdb setup` succeeds

--- docker/influxdb_setup/entrypoint.sh
+++ docker/influxdb_setup/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-cmd="influx setup -f \
+ping="influx ping --host http://influxdb:8086"
+
+while ! eval $ping; do sleep 3; done
+
+influx setup -f \
     --host http://influxdb:8086 \
     -o ${INFLUXDB_ORG} \
     -b ${INFLUXDB_BUCKET} \
     -u ${ADMIN_USER} \
     -p ${ADMIN_PSWD} \
-    -t ${INFLUXDB_TOKEN}"
-
-while ! eval $cmd; do sleep 3; done
+    -t ${INFLUXDB_TOKEN}
