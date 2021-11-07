923cb88200432d7173ab56ac00b3c14f4df9bc19
Calvin McLean <calvinlmc@gmail.com>
2021-11-06 19:10:18 -0700

    Set garden-controller hostname
    Also replaced uses of Serial.print that don't work with printf

--- garden-controller/mqtt.ino
+++ garden-controller/mqtt.ino
@@ -29,22 +29,18 @@ void setupMQTT() {
 
 void setupWifi() {
     delay(10);
-    // We start by connecting to a WiFi network
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(SSID);
+    printf("Connecting to "SSID" as "GARDEN_NAME"-controller\n");
+
+    WiFi.setHostname(GARDEN_NAME"-controller");
 
     WiFi.begin(SSID, PASSWORD);
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
-        Serial.print(".");
+        printf(".");
     }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    printf("Wifi connected...\n");
 }
 
 /*
