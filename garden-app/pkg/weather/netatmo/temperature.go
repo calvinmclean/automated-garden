45e2e62b369ce8dee152d8e42b8aae3b0014f4b4
Calvin McLean <calvinlmc@gmail.com>
2023-01-25 23:08:56 -0700

    Add metrics to WeatherClient and fix cache

--- garden-app/pkg/weather/netatmo/temperature.go
+++ garden-app/pkg/weather/netatmo/temperature.go
@@ -1,10 +1,7 @@
 package netatmo
 
 import (
-	"fmt"
 	"time"
-
-	"github.com/patrickmn/go-cache"
 )
 
 const minTemperatureInterval = 72 * time.Hour
@@ -12,18 +9,12 @@ const minTemperatureInterval = 72 * time.Hour
 // GetAverageHighTemperature returns the average daily high temperature between the given time and the end of
 // yesterday (since daily high can be misleading if queried mid-day)
 func (c *Client) GetAverageHighTemperature(since time.Duration) (float32, error) {
-	now := time.Now()
-	cacheKey := fmt.Sprintf("avg_temp_%d", now.Unix())
-	cachedData, found := c.responseCache.Get(cacheKey)
-	if found {
-		return cachedData.(float32), nil
-	}
-
 	// Time to check since must always be at least 3 days
 	if since < minTemperatureInterval {
 		since = minTemperatureInterval
 	}
 
+	now := time.Now()
 	beginDate := now.Add(-since).Truncate(time.Hour)
 	beginDate = time.Date(beginDate.Year(), beginDate.Month(), beginDate.Day()-1, 23, 59, 59, 0, time.Local)
 	// Since we are looking at daily max temp, get time all the way to very end of yesterday
@@ -33,8 +24,6 @@ func (c *Client) GetAverageHighTemperature(since time.Duration) (float32, error)
 	if err != nil {
 		return 0, err
 	}
-
-	c.responseCache.Set(cacheKey, temperatureData.Average(), cache.DefaultExpiration)
 
 	return temperatureData.Average(), nil
 }
