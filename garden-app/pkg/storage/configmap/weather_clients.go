3b1013d565c9ca5677c1ccd3dac03b499cc6bff4
Calvin McLean <calvinlmc@gmail.com>
2023-04-01 19:47:24 -0700

    Add configmap storage client

---
+++ garden-app/pkg/storage/configmap/weather_clients.go
@@ -0,0 +1,70 @@
+package configmap
+
+import (
+	"github.com/calvinmclean/automated-garden/garden-app/pkg/weather"
+	"github.com/rs/xid"
+)
+
+// GetWeatherClient reads the config from storage and then returns the actual Client to reduce code duplication in the
+// worker code that calls it
+func (c *Client) GetWeatherClient(id xid.ID) (weather.Client, error) {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	err := c.update()
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Do i need nil check? write test for it
+	return weather.NewClient(c.data.WeatherClientConfigs[id], func(weatherClientOptions map[string]interface{}) error {
+		// storageCallback will update the options with the value in the input and then save
+		c.m.Lock()
+		defer c.m.Unlock()
+
+		c.data.WeatherClientConfigs[id].Options = weatherClientOptions
+		return c.save()
+	})
+}
+
+func (c *Client) GetWeatherClientConfig(id xid.ID) (*weather.Config, error) {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	err := c.update()
+	if err != nil {
+		return nil, err
+	}
+	return c.data.WeatherClientConfigs[id], nil
+}
+
+func (c *Client) GetWeatherClientConfigs() ([]*weather.Config, error) {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	err := c.update()
+	if err != nil {
+		return nil, err
+	}
+	result := []*weather.Config{}
+	for _, wc := range c.data.WeatherClientConfigs {
+		result = append(result, wc)
+	}
+	return result, nil
+}
+
+func (c *Client) SaveWeatherClientConfig(wc *weather.Config) error {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	c.data.WeatherClientConfigs[wc.ID] = wc
+	return c.save()
+}
+
+func (c *Client) DeleteWeatherClientConfig(id xid.ID) error {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	delete(c.data.WeatherClientConfigs, id)
+	return c.save()
+}
