e4b05e2ffc9d288791fece1c48ca3e6c851f04c3
Calvin McLean <calvinlmc@gmail.com>
2023-05-26 14:38:24 -0700

    Add support for hord redis driver
    Closes #13

--- garden-app/go.mod
+++ garden-app/go.mod
@@ -26,6 +26,7 @@ require (
 )
 
 require (
+	github.com/FZambia/sentinel v1.1.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -42,6 +43,7 @@ require (
 	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
--- garden-app/pkg/storage/kv/kv.go
+++ garden-app/pkg/storage/kv/kv.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/madflojo/hord"
 	"github.com/madflojo/hord/drivers/hashmap"
+	"github.com/madflojo/hord/drivers/redis"
 	"gopkg.in/yaml.v3"
 )
 
@@ -18,6 +19,22 @@ type Client struct {
 }
 
 func NewClient(options map[string]string) (*Client, error) {
+	driver, ok := options["driver"]
+	if !ok {
+		return nil, fmt.Errorf("missing config key 'driver'")
+	}
+
+	switch driver {
+	case "file":
+		return newFileClient(options)
+	case "redis":
+		return newRedisClient(options)
+	default:
+		return nil, fmt.Errorf("invalid KV driver: %s", driver)
+	}
+}
+
+func newFileClient(options map[string]string) (*Client, error) {
 	if _, ok := options["filename"]; !ok {
 		return nil, fmt.Errorf("missing config key 'filename'")
 	}
@@ -49,4 +66,31 @@ func NewClient(options map[string]string) (*Client, error) {
 	}
 
 	return client, nil
+}
+
+func newRedisClient(options map[string]string) (*Client, error) {
+	server, ok := options["server"]
+	if !ok {
+		return nil, fmt.Errorf("missing config key 'server'")
+	}
+
+	db, err := redis.Dial(redis.Config{
+		Server:   server,
+		Password: options["password"],
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error creating database connection: %w", err)
+	}
+
+	err = db.Setup()
+	if err != nil {
+		return nil, fmt.Errorf("error setting up database: %w", err)
+	}
+
+	return &Client{
+		db:        db,
+		options:   options,
+		unmarshal: yaml.Unmarshal,
+		marshal:   yaml.Marshal,
+	}, nil
 }
