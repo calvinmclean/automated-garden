c025a7e1efd6d73aea83115cc21a715960f1019c
Calvin McLean <calvinlmc@gmail.com>
2023-05-26 14:23:53 -0700

    Reduce duplication by creating superset of methods
    - This allows storage methods that are able to rely on other base
      storage interactions to be implemented once instead of per-implementation

--- garden-app/pkg/storage/client.go
+++ garden-app/pkg/storage/client.go
@@ -17,7 +17,17 @@ type Config struct {
 }
 
 // Client is a "generic" interface used to interact with our storage backend (DB, file, etc)
+// It was separated from BaseClient to allow a single implementation of more advanced methods that are
+// able to only rely on existing interface methods (reduces duplication in implementation packages)
 type Client interface {
+	BaseClient
+
+	GetZonesUsingWaterSchedule(xid.ID) ([]*pkg.ZoneAndGarden, error)
+	GetWaterSchedulesUsingWeatherClient(xid.ID) ([]*pkg.WaterSchedule, error)
+}
+
+// BaseClient holds the required methods for interacting with base resources directly in the storage backend
+type BaseClient interface {
 	GetGarden(xid.ID) (*pkg.Garden, error)
 	GetGardens(bool) ([]*pkg.Garden, error)
 	SaveGarden(*pkg.Garden) error
@@ -44,19 +54,76 @@ type Client interface {
 	GetWaterSchedules(bool) ([]*pkg.WaterSchedule, error)
 	SaveWaterSchedule(*pkg.WaterSchedule) error
 	DeleteWaterSchedule(xid.ID) error
-
-	GetZonesUsingWaterSchedule(xid.ID) ([]*pkg.ZoneAndGarden, error)
-	GetWaterSchedulesUsingWeatherClient(xid.ID) ([]*pkg.WaterSchedule, error)
 }
 
 // NewClient will use the config to create and return the correct type of storage client
 func NewClient(config Config) (Client, error) {
+	var client BaseClient
+	var err error
 	switch config.Type {
 	case "YAML", "yaml", "ConfigMap", "configmap":
-		return yaml.NewClient(config.Type, config.Options)
+		client, err = yaml.NewClient(config.Type, config.Options)
 	case "KV", "kv":
-		return kv.NewClient(config.Options)
+		client, err = kv.NewClient(config.Options)
 	default:
 		return nil, fmt.Errorf("invalid type '%s'", config.Type)
 	}
+
+	return &extendedClient{client}, err
+}
+
+type extendedClient struct {
+	BaseClient
+}
+
+// GetZonesUsingWaterSchedule will find all Zones that use this WaterSchedule and return the Zones along with the Gardens they belong to
+func (c *extendedClient) GetZonesUsingWaterSchedule(id xid.ID) ([]*pkg.ZoneAndGarden, error) {
+	gardens, err := c.GetGardens(false)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get all Gardens: %w", err)
+	}
+
+	results := []*pkg.ZoneAndGarden{}
+	for _, g := range gardens {
+		zones, err := c.GetZones(g.ID, false)
+		if err != nil {
+			return nil, fmt.Errorf("unable to get all Zones for Garden %q: %w", g.ID, err)
+		}
+
+		for _, z := range zones {
+			for _, wsID := range z.WaterScheduleIDs {
+				if wsID == id {
+					results = append(results, &pkg.ZoneAndGarden{Zone: z, Garden: g})
+				}
+			}
+		}
+	}
+
+	return results, nil
+}
+
+// GetWaterSchedulesUsingWeatherClient will return all WaterSchedules that rely on this WeatherClient
+func (c *extendedClient) GetWaterSchedulesUsingWeatherClient(id xid.ID) ([]*pkg.WaterSchedule, error) {
+	waterSchedules, err := c.GetWaterSchedules(false)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get all WaterSchedules: %w", err)
+	}
+
+	results := []*pkg.WaterSchedule{}
+	for _, ws := range waterSchedules {
+		if ws.HasWeatherControl() {
+			if ws.HasRainControl() {
+				if ws.WeatherControl.Rain.ClientID == id {
+					results = append(results, ws)
+				}
+			}
+			if ws.HasTemperatureControl() {
+				if ws.WeatherControl.Temperature.ClientID == id {
+					results = append(results, ws)
+				}
+			}
+		}
+	}
+
+	return results, nil
 }
--- garden-app/pkg/storage/yaml/water_schedules.go
+++ garden-app/pkg/storage/yaml/water_schedules.go
@@ -1,8 +1,6 @@
 package yaml
 
 import (
-	"fmt"
-
 	"github.com/calvinmclean/automated-garden/garden-app/pkg"
 	"github.com/rs/xid"
 )
@@ -72,56 +70,4 @@ func (c *Client) DeleteWaterSchedule(id xid.ID) error {
 
 	delete(c.data.WaterSchedules, id)
 	return c.save()
-}
-
-// GetZonesUsingWaterSchedule ...
-func (c *Client) GetZonesUsingWaterSchedule(id xid.ID) ([]*pkg.ZoneAndGarden, error) {
-	gardens, err := c.GetGardens(false)
-	if err != nil {
-		return nil, fmt.Errorf("unable to get all Gardens: %w", err)
-	}
-
-	results := []*pkg.ZoneAndGarden{}
-	for _, g := range gardens {
-		zones, err := c.GetZones(g.ID, false)
-		if err != nil {
-			return nil, fmt.Errorf("unable to get all Zones for Garden %q: %w", g.ID, err)
-		}
-
-		for _, z := range zones {
-			for _, wsID := range z.WaterScheduleIDs {
-				if wsID == id {
-					results = append(results, &pkg.ZoneAndGarden{Zone: z, Garden: g})
-				}
-			}
-		}
-	}
-
-	return results, nil
-}
-
-// GetWaterSchedulesUsingWeatherClient ...
-func (c *Client) GetWaterSchedulesUsingWeatherClient(id xid.ID) ([]*pkg.WaterSchedule, error) {
-	waterSchedules, err := c.GetWaterSchedules(false)
-	if err != nil {
-		return nil, fmt.Errorf("unable to get all WaterSchedules: %w", err)
-	}
-
-	results := []*pkg.WaterSchedule{}
-	for _, ws := range waterSchedules {
-		if ws.HasWeatherControl() {
-			if ws.HasRainControl() {
-				if ws.WeatherControl.Rain.ClientID == id {
-					results = append(results, ws)
-				}
-			}
-			if ws.HasTemperatureControl() {
-				if ws.WeatherControl.Temperature.ClientID == id {
-					results = append(results, ws)
-				}
-			}
-		}
-	}
-
-	return results, nil
 }
