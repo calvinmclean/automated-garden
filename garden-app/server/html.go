09af4e6e3d6684c14950b2ce7594edf77015955a
Calvin McLean <calvinlmc@gmail.com>
2023-11-03 20:20:09 -0700

    Add HTML Responder and implement HTML for Gardens

---
+++ garden-app/server/html.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"bytes"
+	"html/template"
+)
+
+type HTMLer interface {
+	HTML() string
+}
+
+func renderHTML(htmler HTMLer, data any) string {
+	templates := template.Must(template.New("innerHTML").Parse(htmler.HTML()))
+
+	templates = template.Must(templates.New("GardenApp").Parse(`<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Garden App</title>
+    <style>
+        @import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css";
+        @import "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css";
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-md bg-success" data-bs-theme="light">
+<div class="container-fluid"><a class="navbar-brand" href="#/gardens">Garden App</a> <button
+		class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+	<div class="navbar-collapse collapse show" style="">
+		<ul class="ms-auto navbar-nav">
+			<li class="nav-item"><a href="#/gardens" class="nav-link">Gardens</a></li>
+			<li class="nav-item"><a href="#/water_schedules" class="nav-link">Water Schedules</a></li>
+			<li class="nav-item"><a href="#/weather_clients" class="nav-link">Weather Clients</a></li>
+		</ul>
+	</div>
+</div>
+</nav>
+
+{{template "innerHTML" .}}
+
+</body>
+</html>`))
+
+	var renderedOutput bytes.Buffer
+	err := templates.Execute(&renderedOutput, data)
+	if err != nil {
+		panic(err)
+	}
+
+	return renderedOutput.String()
+}
